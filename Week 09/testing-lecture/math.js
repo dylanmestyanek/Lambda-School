ebdde90acda45646f6faf25cc8562ba19a3d9e30
Dylan Mestyanek <mestyanekdylan@gmail.com>
2019-10-17 10:01:35 -0500

    Remove week 09

--- Week 09/testing-lecture/math.js
+++
@@ -1,7 +0,0 @@
-// these two functions can be considered "unit"
-// They are isolated and easy to test
-
-const sum = (a, b) => a + b;
-const subtract = (a, b) => a - b;
-
-module.exports = { sum, subtract }
